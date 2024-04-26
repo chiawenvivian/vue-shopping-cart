1529ceb5c5015d1be77eaa3860abab00e82f4291
chiawenvivian <chiawenvivian@gmail.com>
2024-04-27 02:29:08 +0800

    shopping-cart重新做

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/shopping-cart/',
+  base: '/vue-shopping-cart/',
   plugins: [
     vue(),
   ],
